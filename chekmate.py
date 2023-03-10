ce28b3aa79aaa55d64eb78ec482a777fbdc0c06b
Bigggor <127144372+Bigggor1@users.noreply.github.com>
2023-03-11 00:05:08 +0300

    yandex_chekmate3

--- chekmate.py
+++ chekmate.py
@@ -8,7 +8,12 @@ def board(num, size):
     new_color = (0, 0, 0)  
     new_image = Image.new("RGB", (size * num, size * num), new_color)
     pixels = new_image.load()
-    for i in range(num):        
+    for i in range(num):
+        if num % 2 == 0:
+            if z == 1:
+                z = 0
+            else:
+                z = 1            
         for j in range(num):
             if z == 1:
                 z = 0

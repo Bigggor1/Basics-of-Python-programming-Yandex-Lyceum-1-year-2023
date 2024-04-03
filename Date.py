c79f39b6df33068a9890b21cdbf3567b99fa5d2b
Bigggor <127144372+Bigggor1@users.noreply.github.com>
2024-04-03 21:41:47 +0300

    а

--- Date.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-
-
-class Date:
-    def __init__(self, m, d):
-        self.my_date = dt.datetime(1999, m, d, 0, 0, 0)
-    
-    def __sub__(self, other):
-        return (self.my_date - other.my_date).days
--- ReversedList.py
+++
@@ -1,9 +0,0 @@
-class ReversedList:
-    def __init__(self, lst):
-        self.lst = lst
-    
-    def __len__(self):
-        return len(self.lst)
-    
-    def __getitem__(self, key):
-        return self.lst[0 - key - 1]
